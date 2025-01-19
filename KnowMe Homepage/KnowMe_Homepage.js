d27ff831884c48b6a31eda6c0536916dee28b6b5
tickbext <116374519+tickbext@users.noreply.github.com>
2025-01-19 21:09:16 +0100

    updated to the latest version

---
+++ KnowMe Homepage/KnowMe_Homepage.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
